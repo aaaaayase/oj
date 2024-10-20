d918d59dec1c5babd1480a0b005c49ec260b82ff
ColdGoose77 <2284858868@qq.com>
2024-10-20 16:17:59 +0800

    up

--- oj-modules/oj-system/src/main/java/com/yun/system/OjSystemApplication.java
+++ oj-modules/oj-system/src/main/java/com/yun/system/OjSystemApplication.java
@@ -1,5 +1,6 @@
 package com.yun.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@MapperScan("com.yun.**.mapper")
 public class OjSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(OjSystemApplication.class, args);
